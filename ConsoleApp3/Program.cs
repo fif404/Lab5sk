730b59903994507a7660e3908f2cb8c23dc4f858
wsr <wsr@DESKTOP-4D6TPP3>
2023-02-04 12:25:10 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите строку : ");
+String sl = Console.ReadLine();
+char[] sym = sl.ToCharArray();
+if (IsPalindrom(sym))
+    Console.WriteLine("Палиндром");
+else
+    Console.WriteLine("Не палиндром");
+        
+        static bool IsPalindrom(char[] word)
+        {
+    for (int i = 0; i < word.Length / 2; i++)
+        if (word[i] != word[word.Length - 1 - i])
+            return false;
+    return true;
+        }
+        
